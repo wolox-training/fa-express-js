70cc118b2bdb1a7fcd5eb17fd9e52cb3814f3c07
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-25 14:58:38 -0500

    fixing comments

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,16 +1,14 @@
 const { getAlbums, getPhotos } = require('../services/albums');
 
-exports.listAlbums = (req, res, next) => {
+exports.listAlbums = (req, res, next) =>
   getAlbums()
-    .then(response => res.status(200).send(response.data))
+    .then(response => res.send(response.data))
     .catch(error => next(error));
-};
 
-exports.getPhotos = (req, res, next) => {
+exports.getPhotos = (req, res, next) =>
   getPhotos(req.params.id)
     .then(response => {
       const photos = response.data.filter(album => album.albumId.toString() === req.params.id);
-      res.status(200).send(photos);
+      res.send(photos);
     })
     .catch(error => next(error));
-};
--- app/services/albums.js
+++ app/services/albums.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
 const errors = require('../errors');
-
-exports.getAlbums = () => axios.get(`${process.env.ALBUMS_URL}albums/`);
+const config = require('../../config');
+exports.getAlbums = () => axios.get(`${config.common.api.albumsBaseUrl}albums/`);
 
 exports.getPhotos = id =>
   axios
-    .get(`${process.env.ALBUMS_URL}albums/${id}/photos`)
-    .then(response => Promise.resolve(response))
-    .catch(() => Promise.reject(errors.badRequestError('Server is unavailable')));
+    .get(`${config.common.api.albumsBaseUrl}albums/${id}/photos`)
+    .catch(() => Promise.reject(errors.defaultError('Server is unavailable')));
