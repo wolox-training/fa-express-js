7d5b1a5d026a670989c248919aa49d25313efdc0
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-29 10:00:38 -0500

    Using snakecase for names and adding id to model

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, unique: true, autoIncrement: true, allowNull: false },
     name: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
+    last_name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false }
   });
--- migrations/migrations/20190726161522-user-migration.js
+++ migrations/migrations/20190726161522-user-migration.js
@@ -5,7 +5,7 @@ module.exports = {
     queryInterface.createTable('Users', {
       id: { type: Sequelize.INTEGER, unique: true, autoIncrement: true, allowNull: false },
       name: { type: Sequelize.STRING, allowNull: false },
-      lastName: { type: Sequelize.STRING, allowNull: false },
+      last_name: { type: Sequelize.STRING, allowNull: false },
       email: { type: Sequelize.STRING, allowNull: false, unique: true },
       password: { type: Sequelize.STRING, allowNull: false }
     }),
