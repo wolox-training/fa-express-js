2d7fd8a6fdcf9aff99659d9d580e690865387487
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-29 10:50:22 -0500

    Changing table name in migrations

--- app/models/user.js
+++ app/models/user.js
@@ -6,6 +6,6 @@ module.exports = (sequelize, DataTypes) => {
     last_name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false }
-  });
+  }, { tableName: 'users' });
   return User;
 };
--- migrations/migrations/20190726161522-user-migration.js
+++ migrations/migrations/20190726161522-user-migration.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Users', {
+    queryInterface.createTable('users', {
       id: { type: Sequelize.INTEGER, unique: true, autoIncrement: true, allowNull: false, primaryKey: true },
       name: { type: Sequelize.STRING, allowNull: false },
       last_name: { type: Sequelize.STRING, allowNull: false },
       email: { type: Sequelize.STRING, allowNull: false, unique: true },
       password: { type: Sequelize.STRING, allowNull: false }
     }),
-  down: queryInterface => queryInterface.dropTable('Users')
+  down: queryInterface => queryInterface.dropTable('users')
 };
