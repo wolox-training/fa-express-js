814e7961af7613cf64e33dd3cf484066403f3b27
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-29 10:08:11 -0500

    Adding primary key property in users id

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, unique: true, autoIncrement: true, allowNull: false },
+    id: { type: DataTypes.INTEGER, unique: true, autoIncrement: true, allowNull: false, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false },
     last_name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
