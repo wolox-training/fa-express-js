f16c5331d9ca871dcba3d4abe7688d2de3a3d668
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-29 07:49:43 -0500

    Removing id from user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      autoIncrement: true,
-      allowNull: false
-    },
     name: { type: DataTypes.STRING, allowNull: false },
     lastName: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
