cde2aefcebd43bd91070fd4b4301214b8e55ebc8
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-29 09:16:58 -0500

    Renaming the table name from user to users

--- migrations/migrations/20190726161522-user-migration.js
+++ migrations/migrations/20190726161522-user-migration.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('user', {
+    queryInterface.createTable('Users', {
       id: { type: Sequelize.INTEGER, unique: true, autoIncrement: true, allowNull: false },
       name: { type: Sequelize.STRING, allowNull: false },
       lastName: { type: Sequelize.STRING, allowNull: false },
       email: { type: Sequelize.STRING, allowNull: false, unique: true },
       password: { type: Sequelize.STRING, allowNull: false }
     }),
-  down: queryInterface => queryInterface.dropTable('user')
+  down: queryInterface => queryInterface.dropTable('Users')
 };
