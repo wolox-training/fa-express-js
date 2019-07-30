1da57e0d471d34a845e957f54bc9f7c76550a997
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-30 15:57:02 -0500

    adding unit tests for users endpoints

---
+++ test/user.spec.js
@@ -0,0 +1,62 @@
+const request = require('supertest');
+const app = require('../app');
+
+const user = {
+  name: 'Joe',
+  last_name: 'Doe',
+  email: 'joedoe@wolox.co',
+  password: 'holamundo2019'
+};
+
+describe('User', () => {
+  it('Service responds with 200', async () => {
+    await request(app)
+      .post('/users')
+      .send(user)
+      .expect(200);
+  });
+
+  it('Service responds with 400', async () => {
+    const user2 = {
+      name: 'Felipe',
+      last_name: 'Montoya',
+      email: 'joedoe@wolox.co',
+      password: 'holamundo2010'
+    };
+    await request(app)
+      .post('/users')
+      .send(user);
+    await request(app)
+      .post('/users')
+      .send(user2)
+      .expect(400);
+  });
+
+  it('Service responds with 400 for bad password', async () => {
+    const user_password = {
+      name: 'Felipe',
+      last_name: 'Montoya',
+      email: 'joedoe@wolox.co',
+      password: 'holamundo'
+    };
+
+    await request(app)
+      .post('/users')
+      .send(user_password)
+      .expect(400);
+  });
+
+  it('Service responds with 400 for empty params', async () => {
+    const user_params = {
+      name: '',
+      last_name: 'Montoya',
+      email: 'joedoe@wolox.co',
+      password: 'holamundo'
+    };
+
+    await request(app)
+      .post('/users')
+      .send(user_params)
+      .expect(400);
+  });
+});
