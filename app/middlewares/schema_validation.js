448c6c3fba76145c825a8753bc13f65da3100455
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-07-31 15:12:11 -0500

    Refactoring the error messages from the schemas

--- app/middlewares/schema_validation.js
+++ app/middlewares/schema_validation.js
@@ -1,11 +1,7 @@
 const { checkSchema, validationResult } = require('express-validator/check');
 
-const errors = require('../errors');
-
 const checkValidationResult = (req, res, next) =>
-  validationResult(req).isEmpty()
-    ? next()
-    : next(errors.badRequestError(validationResult(req).errors[0].msg));
+  validationResult(req).isEmpty() ? next() : next(validationResult(req).errors[0].msg);
 
 const validate = schema => checkSchema(schema);
 
--- app/schemas/user.js
+++ app/schemas/user.js
@@ -1,29 +1,36 @@
 const { User } = require('../models');
+const errors = require('../errors');
 
 exports.user_schema = {
   email: {
     in: ['body'],
     matches: {
       options: /^[a-zA-Z0-9]+@wolox.([A-Za-z])+$/,
-      errorMessage: 'The email must have the wolox domain'
+      errorMessage: errors.badRequestError('The email must have the wolox domain')
     },
     custom: {
-      options: value => User.findAll({ where: { email: value } }).then(users => !(users.length > 0)),
-      errorMessage: 'Email must be unique'
+      options: value =>
+        User.findAll({ where: { email: value } }).then(users =>
+          users.length > 0
+            ? Promise.reject(errors.badRequestError('Email must be unique'))
+            : Promise.resolve(true)
+        )
     }
   },
   password: {
     in: ['body'],
     matches: {
       options: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/,
-      errorMessage: 'The password must have letters and numbers and has to be at least 8 chars long'
+      errorMessage: errors.badRequestError(
+        'The password must have letters and numbers and has to be at least 8 chars long'
+      )
     }
   },
 
   name: {
     in: ['body'],
     isLength: {
-      errorMessage: 'The name cannot be empty',
+      errorMessage: errors.badRequestError('The name cannot be empty'),
       options: { min: 2 }
     }
   },
@@ -31,7 +38,7 @@ exports.user_schema = {
   last_name: {
     in: ['body'],
     isLength: {
-      errorMessage: 'The last name cannot be empty',
+      errorMessage: errors.badRequestError('The last name cannot be empty'),
       options: { min: 2 }
     }
   }
