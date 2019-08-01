1c4b79600b686f041a6103560579474cc90f2b8f
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-08-01 13:37:25 -0500

    Generating an error message with all the errors

--- app/middlewares/schema_validation.js
+++ app/middlewares/schema_validation.js
@@ -2,7 +2,7 @@ const { checkSchema, validationResult } = require('express-validator/check');
 const errors = require('../errors');
 
 const checkValidationResult = (req, res, next) => {
-  const error = validationResult(req).errors.map(error => error.msg);
+  const error = validationResult(req).errors.map(err => err.msg);
   return validationResult(req).isEmpty() ? next() : next(errors.badRequestError(error));
 };
 
