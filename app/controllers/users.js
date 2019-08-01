28cb156ff653411d90a43d38be53eab159d48b57
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-08-01 13:59:38 -0500

    Responding with the user when created successfully

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,5 +2,5 @@ const { createUser } = require('../services/users');
 
 exports.createUser = (req, res, next) =>
   createUser(req.body)
-    .then(() => res.send({ message: 'User Created' }))
+    .then(user => res.send({ name: user.name, last_name: user.last_name, email: user.email }))
     .catch(next);
