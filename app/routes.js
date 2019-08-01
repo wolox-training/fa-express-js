716f7447fe42de6832bd94d7540a2e7b43eeacf2
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-08-01 08:11:52 -0500

    Renaming constant export and renaming the services function for creating user

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { listAlbums, getPhotos } = require('./controllers/albums');
 const { setUser } = require('./controllers/users');
-const { user_schema } = require('./schemas/user');
+const { userSchema } = require('./schemas/user');
 const { validateSchema } = require('./middlewares/schema_validation');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', listAlbums);
   app.get('/albums/:id/photos', getPhotos);
-  app.post('/users', [validateSchema(user_schema)], setUser);
+  app.post('/users', [validateSchema(userSchema)], setUser);
 };
--- app/services/users.js
+++ app/services/users.js
@@ -2,7 +2,8 @@ const { User } = require('../models');
 const errors = require('../errors');
 const bcrypt = require('bcrypt');
 const { SALTROUNDS } = require('../constants');
-exports.setUser = user =>
+
+exports.createUser = user =>
   bcrypt.hash(user.password, SALTROUNDS).then(hash => {
     user.password = hash;
     return User.create(user).catch(error => Promise.reject(errors.databaseError(error.message)));
