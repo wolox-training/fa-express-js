920f0c1cee9845551698c5ea368a55c52f5fc6ec
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-08-01 16:48:39 -0500

    Ussing logger in the sign-in service

--- app/services/users.js
+++ app/services/users.js
@@ -2,15 +2,16 @@ const { User } = require('../models');
 const errors = require('../errors');
 const bcrypt = require('bcryptjs');
 const { SALTROUNDS } = require('../constants');
-
+const logger = require('../logger');
 exports.createUser = user =>
   bcrypt.hash(user.password, SALTROUNDS).then(hash => {
     user.password = hash;
     return User.create(user).catch(error => Promise.reject(errors.databaseError(error.message)));
   });
 
-exports.signInUser = signData =>
-  User.findOne({
+exports.signInUser = signData => {
+  logger.info('Trying to sign-in the user...');
+  return User.findOne({
     where: {
       email: signData.email
     }
@@ -19,9 +20,17 @@ exports.signInUser = signData =>
       if (user) {
         return bcrypt
           .compare(signData.password, user.password)
-          .then(res => (res ? Promise.resolve() : Promise.reject(errors.badRequestError('Wrong password!'))))
+          .then(res => {
+            if (res) {
+              logger.info('User exists and passwords are matching');
+              return Promise.resolve();
+            }
+            return Promise.reject(errors.badRequestError('Wrong password!'));
+          })
           .catch(error => Promise.reject(errors.badRequestError(error.message)));
       }
+      logger.error('Email does not exists');
       return Promise.reject(errors.badRequestError('No user with that email'));
     })
     .catch(error => Promise.reject(errors.badRequestError(error.message)));
+};
