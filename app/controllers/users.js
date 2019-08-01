980c7f9ae31d678aee299fadd9e7f2ef191ddbfd
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-08-01 13:52:03 -0500

    Renaming controller function to createUser

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,6 @@
 const { createUser } = require('../services/users');
 
-exports.setUser = (req, res, next) =>
+exports.createUser = (req, res, next) =>
   createUser(req.body)
     .then(() => res.send({ message: 'User Created' }))
     .catch(next);
--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { listAlbums, getPhotos } = require('./controllers/albums');
-const { setUser } = require('./controllers/users');
+const { createUser } = require('./controllers/users');
 const { userSchema } = require('./schemas/user');
 const { validateSchema } = require('./middlewares/schema_validation');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', listAlbums);
   app.get('/albums/:id/photos', getPhotos);
-  app.post('/users', [validateSchema(userSchema)], setUser);
+  app.post('/users', [validateSchema(userSchema)], createUser);
 };
