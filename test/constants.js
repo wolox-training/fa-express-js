1c57f1b8ac8823935d89a1c499ccc21f07b08d1a
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-7.local>
2019-08-05 11:30:55 -0500

    fixing test assertion and adding the test-constants file

---
+++ test/constants.js
@@ -0,0 +1,6 @@
+exports.USER = {
+  name: 'Joe',
+  last_name: 'Doe',
+  email: 'joedoe@wolox.co',
+  password: 'holamundo2019'
+};
--- test/user.spec.js
+++ test/user.spec.js
@@ -1,56 +1,51 @@
 const request = require('supertest');
 const app = require('../app');
+const { User } = require('../app/models');
+const { USER } = require('./constants');
 
-const userCreate = {
-  name: 'Joe',
-  last_name: 'Doe',
-  email: 'joedoe@wolox.co',
-  password: 'holamundo2019'
-};
-
-const userParams = {
-  name: '',
-  last_name: 'Montoya',
-  email: 'joedoe@wolox.co',
-  password: 'holamundo'
-};
-
-const userPassword = {
-  name: 'Felipe',
-  last_name: 'Montoya',
-  email: 'joedoe@wolox.co',
-  password: 'holamundo'
-};
-
-describe('User creation', () => {
-  it('Service responds with 200', async () => {
-    await request(app)
+describe('User Creation', () => {
+  it('Service responds with 200 when user is created correctly', () =>
+    request(app)
       .post('/users')
-      .send(userCreate)
-      .expect(200);
-  });
+      .send(USER)
+      .then(res => {
+        expect(res.statusCode).toEqual(200);
+        return User.findAll({ where: { email: USER.email } }).then(users => {
+          expect(users.length).toEqual(1);
+        });
+      }));
 
-  it('Service responds with 400', async () => {
-    await request(app)
+  it('Service responds with 400 when the email exists', () =>
+    request(app)
       .post('/users')
-      .send(userCreate);
-    await request(app)
-      .post('/users')
-      .send(userCreate)
-      .expect(400);
-  });
+      .send(USER)
+      .then(() =>
+        request(app)
+          .post('/users')
+          .send(USER)
+          .then(res => {
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.message).toEqual('Email must be unique');
+          })
+      ));
 
-  it('Service responds with 400 for bad password', async () => {
-    await request(app)
+  it('Service responds with 400 for bad password', () =>
+    request(app)
       .post('/users')
-      .send(userPassword)
-      .expect(400);
-  });
+      .send({ ...USER, password: 'holamundo' })
+      .then(res => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.message).toEqual(
+          'The password must have letters and numbers and has to be at least 8 chars long'
+        );
+      }));
 
-  it('Service responds with 400 for empty params', async () => {
-    await request(app)
+  it('Service responds with 400 for empty params in body', () =>
+    request(app)
       .post('/users')
-      .send(userParams)
-      .expect(400);
-  });
+      .send({ ...USER, name: '' })
+      .then(res => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.message).toEqual('The name cannot be empty');
+      }));
 });
