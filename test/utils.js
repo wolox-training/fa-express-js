3bbe82293cf3866a6ed74a2bf0055837c3393174
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-9.local>
2019-08-12 15:31:57 -0500

    Removing factory in tests, is not used

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,5 @@
+const { factory } = require('factory-girl');
+const { User } = require('../app/models');
 exports.user = {
   name: 'Joe',
   last_name: 'Doe',
@@ -9,3 +11,10 @@ exports.signInData = {
   email: 'joedoe@wolox.co',
   password: 'holamundo2019'
 };
+
+factory.define('user', User, {
+  name: 'Joe',
+  last_name: 'Doe',
+  email: 'joedoe@wolox.co',
+  password: 'holamundo2019'
+});
