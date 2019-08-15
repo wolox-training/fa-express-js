c7b1632cf2b7a329e3781f36ac58b690f7f7742d
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-9.local>
2019-08-15 11:57:57 -0500

    Adding helper for jwt decoding

--- app/middlewares/session_validation.js
+++ app/middlewares/session_validation.js
@@ -1,17 +1,12 @@
 const errors = require('../errors');
-const jwt = require('jwt-simple');
-const {
-  common: {
-    session: { secret }
-  }
-} = require('../../config');
 const { findUser } = require('../services/users');
+const { decodeJwt } = require('../utils/helpers');
 
 exports.checkSession = async (req, _, next) => {
   const token = req.headers.authorization;
   try {
     if (token) {
-      const { email } = jwt.decode(token, secret);
+      const { email } = decodeJwt(token);
       if (email) {
         const user = await findUser({ email });
         if (user) {
@@ -23,8 +18,6 @@ exports.checkSession = async (req, _, next) => {
     }
     return next(errors.unauthorizedError('Invalid Token'));
   } catch (error) {
-    return error.internal_code === 'database_error'
-      ? next(error)
-      : next(errors.unauthorizedError(error.message));
+    return next(error);
   }
 };
