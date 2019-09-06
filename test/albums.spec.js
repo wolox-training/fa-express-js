71ac28c8a558fd126278ac5609384ddfe0faf26e
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-9.local>
2019-09-06 08:33:13 -0500

    adding unit tests for the buy albums service

--- test/albums.spec.js
+++ test/albums.spec.js
@@ -24,4 +24,46 @@ describe('Purchase Albums', () => {
           })
         );
     }));
+
+  it('Responds with bad request when user buys the album two times', () =>
+    createUser().then(user => {
+      const token = jwt.encode({ email: user.dataValues.email, role: user.dataValues.role }, secret);
+      return request(app)
+        .post('/albums/1')
+        .set('authorization', token)
+        .then(() =>
+          request(app)
+            .post('/albums/1')
+            .set('authorization', token)
+            .then(res => {
+              expect(res.status).toBe(400);
+              expect(res.body.message).toBe('You have bought this book');
+            })
+        );
+    }));
+
+  it('Responds with success when two users buy the same album', () =>
+    createUser().then(user => {
+      const token = jwt.encode({ email: user.dataValues.email, role: user.dataValues.role }, secret);
+      return request(app)
+        .post('/albums/1')
+        .set('authorization', token)
+        .then(() =>
+          createUser().then(user2 => {
+            const token2 = jwt.encode({ email: user2.dataValues.email, role: user2.dataValues.role }, secret);
+            return request(app)
+              .post('/albums/1')
+              .set('authorization', token2)
+              .then(response =>
+                getPurchasedAlbums({ album: 1 }).then(purchasedAlbums => {
+                  expect(response.status).toBe(200);
+                  expect(purchasedAlbums[0].dataValues.user).toBe(user.email);
+                  expect(purchasedAlbums[1].dataValues.user).toBe(user2.email);
+                  expect(purchasedAlbums[1].dataValues.album).toBe(response.body.albumPurchased.album);
+                  expect(purchasedAlbums[0].dataValues.album).toBe(response.body.albumPurchased.album);
+                })
+              );
+          })
+        );
+    }));
 });
